d01d33f2fc38cf20e4e9e50adf9d109010630070
lucaperullo <lucaperullo@outlook.it>
2025-02-01 13:48:40 +0100

    fix: be

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: "https://relate15-be.onrender.com",
+        target: "https://relate15.onrender.com",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
